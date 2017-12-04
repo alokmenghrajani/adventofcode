b32c81f72e0de8429e15823a0ebd80341b3fd16e
Alok Menghrajani <alok@squareup.com>
2017-12-04 12:25:52 -0800

    minor rename

--- src/year_2015/day01.rs
+++ src/year_2015/day01.rs
@@ -1,14 +1,14 @@
 pub fn run(input: &[u8]) {
-    let part1 = part1(input);
+    let part1 = solve_part1(input);
     println!("part1: {}", part1);
     assert_eq!(part1, 232);
 
-    let part2 = part2(input);
+    let part2 = solve_part2(input);
     println!("part2: {}", part2);
     assert_eq!(part2, 1783);
 }
 
-fn part1(buf: &[u8]) -> i64 {
+fn solve_part1(buf: &[u8]) -> i64 {
     let mut floor: i64 = 0;
     // loop over every character
     for i in 0..buf.len() {
@@ -25,7 +25,7 @@ fn part1(buf: &[u8]) -> i64 {
     return floor;
 }
 
-fn part2(buf: &[u8]) -> usize {
+fn solve_part2(buf: &[u8]) -> usize {
     let mut floor: i64 = 0;
     // loop over every character
     for i in 0..buf.len() {
