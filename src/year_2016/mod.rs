3dd50c3e9a0ad3756d2ab73bcf39b249203c3e79
Alok Menghrajani <alok@squareup.com>
2017-12-16 23:41:19 -0800

    some inputs

--- src/year_2016/mod.rs
+++ src/year_2016/mod.rs
@@ -8,12 +8,17 @@ pub mod day07;
 pub mod day08;
 pub mod day09;
 pub mod day10;
-// pub mod day12;
+// day11
+// pub mod day12; (doesn't build on relase)
+// day13
 pub mod day14;
 pub mod day15;
 pub mod day16;
+// day17
 pub mod day18;
 pub mod day19;
 pub mod day20;
 pub mod day21;
 // pub mod day23;
+// day24
+// day25
